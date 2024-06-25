1c6cb505278580fdd7284762504fa3da76572528
prince kumar <agarwal8789@gmail.com>
2024-06-25 18:15:52 +0530

    feat: Add PostCard component
    This commit adds the PostCard component to the project. The component is responsible for rendering a post card with the necessary information. It also includes the necessary styling to ensure a visually appealing appearance.Day3

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,26 @@
 import React,{useState} from 'react'
 import { useSelector } from 'react-redux'
-import { TopBar,ProfileCard,FriendsCard, CustomButton } from '../components'
+import { TopBar,ProfileCard,FriendsCard, CustomButton, TextInput, Loading,PostCard } from '../components'
 import { Link } from 'react-router-dom'
-import { suggest,requests } from '../assets/data'
+import { suggest,requests,posts } from '../assets/data'
 import { NoProfile } from '../assets'
-import { BsPersonFillAdd } from 'react-icons/bs'
+import { BsFiletypeGif, BsPersonFillAdd } from 'react-icons/bs'
+import { BiImages, BiSolidVideo } from 'react-icons/bi'
+import { useForm } from 'react-hook-form'
 const Home = () => {
   const {user} = useSelector((state) => state.user);
   const [friendRequest, setFriendRequest] = useState(requests);
   const [suggestedFriends, setSuggestedFriends] = useState(suggest);
+  const [errMsg,setErrMsg]=useState("");
+  const [file,setFile]=useState(null);
+  const [posting,setPosting]=useState(false);
+  const [loading,setLoading]=useState(false);
+  const{
+    register,
+    handleSubmit,
+    formState: { errors },
+  }=useForm();
+  const handlePostSubmit=async(data)=>{}
   return (
     <div className='home w-full px-0 lg:px-10 pb-20 2xl:px-40 bg-bgColor
                     lg:rounded-lg h-screen overflow-hidden'>
@@ -21,8 +33,81 @@ const Home = () => {
               </div>
               {/*CENTER*/}
               <div className='flex-1 h-full bg-primary px-4 flex flex-col gap-6
-                    overflow-y-auto'>
+                    overflow-y-auto rounded-lg'>
+                <form action="" className='bg-primary px-4 rounded-lg'
+                      onSubmit={handleSubmit(handlePostSubmit)}>
+                  <div className='w-full flex items-center gap-2 py-2 border-b border-[#66666645]'>
+                    <img src={user?.profileUrl??NoProfile} alt="User Image" 
+                          className='w-14 h-14 rounded-full object-cover'/>
+                    <TextInput styles='w-full rounded-full py-5'
+                                placeholder="What's on your mind..."
+                                name='description'
+                                register={register("description",{
+                                  required:"Write something about post",
+                                })}
+                                error={errors.description?errors.description.message:""}
+                    />
+                  </div>
+                  {errMsg && (<span role='alert'
+                        className={`text-sm ${errMsg?.status==='failed'
+                        ?'text-[#f64949fe]':'text-[#1bb55c]'}mt-0.5`}>
+                    </span>)}
+                  <div className='flex items-center justify-between py-4'>
+                    <label htmlFor="imgUpload"
+                            className='flex items-center gap-1 text-base text-ascent-2
+                                        hover:text-ascent-1 cursor-pointer'>
+                      <input type="file"
+                              onChange={(e)=> setFile(e.target.files[0])}
+                              className='hidden' id='imgUpload'
+                              data-max-size='5120'
+                              accept='.jpg .png .jpeg'
+                      />
+                      <BiImages />
+                      <span>Image</span>
+                    </label>
+                    <label htmlFor="videoUpload"
+                            className='flex items-center gap-1 text-base text-ascent-2
+                                        hover:text-ascent-1 cursor-pointer'>
+                      <input type="file"
+                            data-max-size='5120'
+                            onChange={(e)=>setFile(e.target.files[0])}
+                            className='hidden' id='videoUpload'
+                            accept='.mp4 .wav'/>
+                            <BiSolidVideo/>
+                            <span>Video</span>
+                    </label>
+                    <label htmlFor="vgifUpload"
+                            className='flex items-center gap-1 text-base text-ascent-2
+                                      hover:text-ascent-1 cursor-pointer'>
+                      <input type="file"
+                            data-max-size='5120'
+                            onChange={(e)=>setFile(e.target.files[0])}
+                            className='hidden' id='vgifUpload'
+                            accept='.gif'/>
+                            <BsFiletypeGif/>
+                            <span>Gif</span>
 
+                    </label>
+                    <div>
+                      { posting ?(<Loading/>):(
+                        <CustomButton
+                            type='submit' title='Post'
+                            containerStyles='bg-[#0444a4] text-white py-1 px-6
+                                            rounded-full font-semibold text-sm'
+                        />
+                      )}
+                    </div>
+                  </div>
+                </form>
+                {loading ? (<Loading/>):posts?.length>0 ?(
+                  posts?.map((post)=>(
+                    <PostCard key={post?._id} post={post}
+                      user={user} deletePost={()=>{}}
+                      likePost={()=>{}}/>
+                  ))
+                ):(<div className='flex w-full h-full items-center justify-center'>
+                  <p className='text-lg text-ascent-2'>Not Post Available</p>
+                </div>)}
               </div>
               {/* RIGHT */}
               <div className='hidden w-1/4 h-full lg:flex flex-col gap-8
