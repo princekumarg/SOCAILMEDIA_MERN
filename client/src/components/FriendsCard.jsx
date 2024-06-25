e1e30b0cc6b6b0714d2963da2d6f937dab4647d1
prince kumar <agarwal8789@gmail.com>
2024-06-25 12:47:00 +0530

    chore: Update FriendsCard component
    This commit updates the FriendsCard component to include a dynamic list of friends and their profiles. It also adds styling to improve the visual appearance of the component.

--- client/src/components/FriendsCard.jsx
+++ client/src/components/FriendsCard.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { NoProfile } from '../assets'
 
-const FriendsCard = () => {
+const FriendsCard = ({friends}) => {
   return (
     <div>
-      FriendsCard
+      <div className='w-full bg-primary shadow-sm rounded-lg px-6 py-5'>
+        <div className='flex items-center justify-between text-ascent-1 pb-2
+                        border-b border-[#66666645]'>
+            <span>Friends</span>
+            <span>{friends?.length}</span>
+
+        </div>
+        <div className='w-full flex flex-col gap-4 pt-4'>
+          {
+            friends?.map((friend)=>(
+              <Link to={'/profile/'+friend?._id} key={friend?._id} 
+                    className='w-full flex gap-4 items-center cursor-pointer'>
+                  <img src={friend?.profileUrl??NoProfile} alt={friend?.firstName} 
+                        className='w-10 h-10 object-cover rounded-full'/>
+                  <div className='flex-1'>
+                    <p className='text-base font-medium text-ascent-1'>{friend?.firstName} {friend?.lastName}</p>
+                    <span>
+                      {friend?.profession ?? "No Profession"}
+                    </span>
+                  </div>
+              </Link>
+            ))
+          }
+        </div>
+      </div>
     </div>
   )
 }
